4e4ee6d472340c97b10cad5510e8925e30dad3bf
ABHI-YADAV <152514021+Abhi-755@users.noreply.github.com>
2024-08-09 13:10:59 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,31 @@
-$(document).ready(function(){
-	$('#action_menu_btn').click(function(){
-		$('.action_menu').toggle();
-	});
-});
+// MESSAGE INPUT
+const textarea = document.querySelector('.chatbox-message-input')
+const chatboxForm = document.querySelector('.chatbox-message-form')
+
+textarea.addEventListener('input', function () {
+	let line = textarea.value.split('\n').length
+
+	if(textarea.rows < 6 || line < 6) {
+		textarea.rows = line
+	}
+
+	if(textarea.rows > 1) {
+		chatboxForm.style.alignItems = 'flex-end'
+	} else {
+		chatboxForm.style.alignItems = 'center'
+	}
+})
+
+
+
+// TOGGLE CHATBOX
+const chatboxToggle = document.querySelector('.chatbox-toggle')
+const chatboxMessage = document.querySelector('.chatbox-message-wrapper')
+
+chatboxToggle.addEventListener('click', function () {
+	chatboxMessage.classList.toggle('show')
+})
+
+
+
+// DROPDOWN TOGGLE
